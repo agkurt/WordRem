832efaf55d6148c314a2f91462140a6771b56a57
Ahmet Göktürk Kurt <ahmetgokturkkurt3@gmail.com>
2023-10-12 01:14:41 +0300

    Initial Commit

---
+++ reminderToWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reminderToWords
+//
+//  Created by Ahmet Göktürk Kurt on 12.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
